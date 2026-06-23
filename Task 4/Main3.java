868cd8177a0644ce3c35a6dde4f81b4ff8ae464e
mahmudafariha <140585966+mahmudafariha@users.noreply.github.com>
2026-06-23 10:36:55 +0600

    Update Main3.java

--- Task 4/Main3.java
+++ Task 4/Main3.java
@@ -1,1 +1,49 @@
+interface Printer {
+    void printDocument();
+}
 
+interface Scanner {
+    void scanDocument();
+}
+
+interface FaxMachine {
+    void faxDocument();
+}
+
+
+class MultiFunctionCopier implements Printer, Scanner, FaxMachine {
+
+
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+
+
+    public void scanDocument() {
+        System.out.println("Scanning...");
+    }
+
+
+    public void faxDocument() {
+        System.out.println("Faxing...");
+    }
+}
+
+class BasicPrinter implements Printer {
+
+    public void printDocument() {
+        System.out.println("Printing...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        BasicPrinter printer = new BasicPrinter();
+        printer.printDocument();
+
+        MultiFunctionCopier copier = new MultiFunctionCopier();
+        copier.printDocument();
+        copier.scanDocument();
+        copier.faxDocument();
+    }
+}
