7d6f5c655f3af1add9d910e72ff93711f0c592f7
mahmudafariha <140585966+mahmudafariha@users.noreply.github.com>
2026-06-23 10:41:12 +0600

    Update Main4.java

--- Task 4/Main4.java
+++ Task 4/Main4.java
@@ -1,1 +1,24 @@
-
+interface MessageSender {
+    void sendMessage(String message);
+}
+class EmailSender implements MessageSender {
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+class NotificationService {
+    private MessageSender messageSender;
+    public NotificationService(MessageSender messageSender) {
+        this.messageSender = messageSender;
+    }
+     public void alertUser(String msg) {
+        messageSender.sendMessage(msg);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        MessageSender sender = new EmailSender();
+        NotificationService service = new NotificationService(sender);
+        service.alertUser("Hello User!");
+    }
+}
