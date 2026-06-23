09b1ec764d3ac032b42bfab7209140a7d23d4334
mahmudafariha <140585966+mahmudafariha@users.noreply.github.com>
2026-06-23 10:33:38 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,46 @@
 
+class TeamMember {
+    protected String name;
+
+    public TeamMember(String name) {
+        this.name = name;
+    }
+}
+
+
+interface BonusEligible {
+    void calculateBonus();
+}
+
+
+class Employee extends TeamMember implements BonusEligible {
+
+    public Employee(String name) {
+        super(name);
+    }
+
+    
+    public void calculateBonus() {
+        System.out.println("Calculating standard employee bonus...");
+    }
+}
+
+
+class Contractor extends TeamMember {
+
+    public Contractor(String name) {
+        super(name);
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp = new Employee("John");
+        emp.calculateBonus();
+
+        Contractor contractor = new Contractor("Mike");
+
+        System.out.println("Contractor: " + contractor.name);
+    }
+}
